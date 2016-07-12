e0259a655fb78c923e564d0db9c46b71c6069d20
Sergio <sergio@Developers-MacBook-Pro.local>
2016-07-12 13:16:35 +0200

    Worked on css grid

--- index.php
+++ index.php
@@ -4,10 +4,12 @@
     <div id="content">
         <h1>Main Area</h1>
         <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-        <h1><?php the_title(); ?></h1>
-        <h4>Posted on <?php the_time('F jS, Y') ?></h4>
-        <p><?php the_content(__('(more...)')); ?></p>
-        <hr> <?php endwhile; else: ?>
+        <div class="post">
+            <h1><?php the_title(); ?></h1>
+            <h4>Posted on <?php the_time('F jS, Y') ?></h4>
+            <p><?php the_content(__('(more...)')); ?></p>
+        </div>
+         <?php endwhile; else: ?>
         <p><?php _e('Sorry, no posts matched your criteria.'); ?></p><?php endif; ?>
     </div>
 </div>
