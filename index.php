3f36f50da082654c2b45386917df2e52ae292a93
Sergio García <garciparedes@gmail.com>
2016-07-12 23:11:53 +0200

    Improved code

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 <?php get_sidebar(); ?>
-<div id="main">
+<main>
     <div id="content">
         <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
             <div class="song" style="background-image: url(<?php echo z_taxonomy_image_url(get_the_category()[0]->term_id); ?> );">
@@ -10,7 +10,5 @@
              <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
         <?php endif; ?>
     </div>
-</div>
-<div id="delimiter">
-</div>
+</main>
 <?php get_footer(); ?>
