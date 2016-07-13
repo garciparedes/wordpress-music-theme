c4abcdd7993c0f5cea455ed1ad78b28497936790
Sergio <sergio@Developers-MacBook-Pro.local>
2016-07-13 09:46:55 +0200

    Improved css

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <div id="content">
         <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
             <div class="song" style="background-image: url(<?php echo z_taxonomy_image_url(get_the_category()[0]->term_id); ?> );">
-                <h1><?php the_title(); ?></h1>
+                <h3 class="stroke"><?php the_title(); ?></h3>
             </div>
          <?php endwhile; else: ?>
              <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
